24c0737f3a27f0fe2462e8b056e1d9c4c75dc1e2
Antony <savchenko3337@gmail.com>
2024-06-19 23:19:28 +0300

    Homework 6

---
+++ homework_6/task1.js
@@ -0,0 +1,21 @@
+//==========function declaration========
+/*
+function perimeter (width, height) {
+    return width * height;
+}
+
+let result = perimeter (3, 5);
+console.log(result);
+*/
+//============function expression========
+/*
+const perimeter2 = function (width2, height2) {
+    return width2 * height2;
+}
+let result2 = perimeter2 (5, 10);
+console.log(result2);
+*/
+//==========Arrow function==============
+const perimeter3 = (width3, height3) => width3 * height3
+let result3 = perimeter3 (3, 5);
+console.log(result3);
---
+++ homework_6/task2.js
@@ -0,0 +1,9 @@
+function isOld (age) {
+    if (age >=18) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+isOld (25);
+isOld (15);
